3ce2d145155e97ed6d79d904b85dfd3be3344b6b
Ragavi126 <97591822+Ragavi126@users.noreply.github.com>
2026-07-11 17:02:32 +0530

    Increase array size from 5 to 6 elements
    increase the size of an array

--- Array.java
+++ Array.java
@@ -12,13 +12,14 @@ public class Array {
 
 	public static void main(String[] args) {
 	
-		String array[]=new String[5];
+		String array[]=new String[6];
 		
 		array[0]="mukesh23";
 		array[1]="anand";
 		array[2]="any";
 		array[3]="hj";
 		array[4]="ghh";
+		array[5]="hgt";
 		
 		System.out.println(array[3]);	
 		
